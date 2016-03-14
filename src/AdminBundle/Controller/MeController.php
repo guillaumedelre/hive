34235eace9c059d518ba1ad8dfbc2222b62c350a
Guillaume Delré <delre.guillaume@gmail.com>
2016-03-14 14:39:38 +0100

    implement fos user provider for authentication

--- src/AdminBundle/Controller/MeController.php
+++ src/AdminBundle/Controller/MeController.php
@@ -17,30 +17,30 @@ class MeController extends Controller
         /** @var User $entity */
         $entity = $this->get('core.repository.user')->findOneByUsername($this->getUser()->getUsername());
 
-        if (null !== $entity) {
-            $this->get('core.form.handler.user')->buildForm($entity);
-
-            if ('POST' === $request->getMethod()) {
-                try {
-                    $this->get('core.form.handler.user')->process($request, $entity);
-                    $this->get('session')->getFlashBag()->add('success', 'The user has been saved.');
-
-                    return $this->redirectToRoute('admin_me_index');
-                } catch (\Exception $e) {
-                    $this->get('session')->getFlashBag()->add('danger', $e->getMessage());
-
-                    return $this->redirectToRoute('admin_me_index');
-                }
-            }
-        } else {
+        if (null === $entity) {
             $this->get('session')->getFlashBag()->add('danger', "User #".$this->getUser()->getUsername()." not found");
             return $this->redirectToRoute('admin_default_index');
         }
 
+        $this->get('core.form.handler.user')->buildForm($entity);
+
+        if ('POST' === $request->getMethod()) {
+            try {
+                $this->get('core.form.handler.user')->process($request, $entity);
+                $this->get('session')->getFlashBag()->add('success', 'The user has been saved.');
+
+                return $this->redirectToRoute('admin_me_index');
+            } catch (\Exception $e) {
+                $this->get('session')->getFlashBag()->add('danger', $e->getMessage());
+
+                return $this->redirectToRoute('admin_me_index');
+            }
+        }
+
         $data = array(
             'pageTitle' => 'Mon profil',
-            "document" => $entity,
-            "form" => $this->get('core.form.handler.document')->getForm()->createView(),
+            "me" => $entity,
+            "form" => $this->get('core.form.handler.user')->getForm()->createView(),
         );
 
         return $this->render('AdminBundle:Me:index.html.twig', $data);
