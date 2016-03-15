19c7ed696d6bdfba0a3586c4b0523da9336d0c8d
Guillaume Delré <delre.guillaume@gmail.com>
2016-03-16 00:05:49 +0100

    front app document is ok

---
+++ src/AppBundle/Controller/DocumentController.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gdelre
+ * Date: 15/03/16
+ * Time: 18:18
+ */
+
+namespace AppBundle\Controller;
+
+
+use CoreBundle\Entity\AbstractEntity;
+use CoreBundle\Entity\Document;
+use CoreBundle\Entity\Hive;
+use CoreBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class DocumentController extends Controller
+{
+    /**
+     * @Route("/{hiveSlug}/documents", name="app_document_index")
+     */
+    public function indexAction(Request $request, $hiveSlug)
+    {
+        $page = $request->query->get('offset') ? $request->query->get('offset') : 0;
+
+        /** @var User $me */
+        $me = $this->get('core.service.me')->getUser();
+
+        if ($hiveSlug !== $me->getHive()->getSlug()) {
+            $this->get('session')->getFlashBag()->add('danger', "L'url demandée est inconnue.");
+
+            return $this->redirectToRoute('app_default_index', ['hiveSlug' => $me->getHive()->getSlug()]);
+        }
+
+        $documents = $this->get('core.repository.document')->getByHive($me->getHive(), $page);
+
+        $categories = $this->get('core.repository.category')->getAllByHive($me->getHive());
+
+        $data = array(
+            'currentPage' => $page,
+            'pageTitle'   => 'Documents',
+            'totalPages'  => ceil(count($this->get('core.repository.document')->getAllByHive($me->getHive())) / 5),
+            'me'          => $this->get('core.service.me')->getUser(),
+            'documents'    => $documents,
+            'categories'  => $categories,
+        );
+
+        return $this->render('AppBundle:Document:index.html.twig', $data);
+    }
+
+    /**
+     * @Route("/{hiveSlug}/documents/categories/{categorySlug}", name="app_document_by_category")
+     */
+    public function byCategoryAction(Request $request, $hiveSlug, $categorySlug)
+    {
+        $page = $request->query->get('offset') ? $request->query->get('offset') : 0;
+
+        /** @var User $me */
+        $me = $this->get('core.service.me')->getUser();
+
+        if ($hiveSlug !== $me->getHive()->getSlug()) {
+            $this->get('session')->getFlashBag()->add('danger', "L'url demandée est inconnue.");
+
+            return $this->redirectToRoute('app_default_index', ['hiveSlug' => $me->getHive()->getSlug()]);
+        }
+
+        $category = $this->get('core.repository.category')->findOneBySlug($categorySlug);
+
+        if (null === $category) {
+            $this->get('session')->getFlashBag()->add('danger', "L'url demandée est inconnue.");
+
+            return $this->redirectToRoute('app_default_index', ['hiveSlug' => $me->getHive()->getSlug()]);
+        }
+
+        $documents = $this->get('core.repository.document')->getByHiveAndCategory($me->getHive(), $category, $page);
+
+        $categories = $this->get('core.repository.category')->getAllByHive($me->getHive());
+
+        $data = array(
+            'currentPage'     => $page,
+            'pageTitle'       => 'Documents',
+            'totalPages'      => ceil(count($this->get('core.repository.document')->getAllByHiveAndCategory($me->getHive(), $category)) / AbstractEntity::DEFAULT_LIMIT_APP),
+            'me'              => $this->get('core.service.me')->getUser(),
+            'documents'        => $documents,
+            'categories'      => $categories,
+            'currentCategory' => $category,
+        );
+
+        return $this->render('AppBundle:Document:bycategory.html.twig', $data);
+    }
+
+}
--- src/CoreBundle/Entity/Repository/DocumentRepository.php
+++ src/CoreBundle/Entity/Repository/DocumentRepository.php
@@ -9,7 +9,89 @@
 namespace CoreBundle\Entity\Repository;
 
 
+use CoreBundle\Entity\AbstractEntity;
+use CoreBundle\Entity\Category;
+use CoreBundle\Entity\Document;
+use CoreBundle\Entity\Hive;
+
 class DocumentRepository extends AbstractRepository
 {
+    /**
+     * @param Hive $hive
+     * @param integer $offset
+     * @return Document[]
+     */
+    public function getByHive(Hive $hive, $offset = 0)
+    {
+        return $this->createQueryBuilder('d')
+            ->innerJoin('d.user', 'u')
+            ->innerJoin('u.hive', 'h')
+            ->where('u.hive = :hive')
+            ->setParameter('hive', $hive)
+            ->setMaxResults(AbstractEntity::DEFAULT_LIMIT_APP)
+            ->setFirstResult($offset)
+            ->orderBy('d.updatedAt', 'DESC')
+            ->getQuery()
+            ->getResult()
+            ;
+    }
 
+    /**
+     * @param Hive $hive
+     * @return Document[]
+     */
+    public function getAllByHive(Hive $hive)
+    {
+        return $this->createQueryBuilder('d')
+            ->innerJoin('d.user', 'u')
+            ->innerJoin('u.hive', 'h')
+            ->where('u.hive = :hive')
+            ->setParameter('hive', $hive)
+            ->orderBy('d.updatedAt', 'DESC')
+            ->getQuery()
+            ->getResult()
+            ;
+    }
+
+    /**
+     * @param Hive $hive
+     * @param integer $offset
+     * @return Document[]
+     */
+    public function getByHiveAndCategory(Hive $hive, Category $category, $offset = 0)
+    {
+        return $this->createQueryBuilder('d')
+            ->innerJoin('d.user', 'u')
+            ->innerJoin('u.hive', 'h')
+            ->where('u.hive = :hive')
+            ->andWhere('d.category = :category')
+            ->setParameter('hive', $hive)
+            ->setParameter('category', $category)
+            ->setMaxResults(AbstractEntity::DEFAULT_LIMIT_APP)
+            ->setFirstResult($offset)
+            ->orderBy('d.updatedAt', 'DESC')
+            ->getQuery()
+            ->getResult()
+            ;
+    }
+
+    /**
+     * @param Hive $hive
+     * @param integer $offset
+     * @return Document[]
+     */
+    public function getAllByHiveAndCategory(Hive $hive, Category $category)
+    {
+        return $this->createQueryBuilder('d')
+            ->innerJoin('d.user', 'u')
+            ->innerJoin('u.hive', 'h')
+            ->where('u.hive = :hive')
+            ->andWhere('d.category = :category')
+            ->setParameter('hive', $hive)
+            ->setParameter('category', $category)
+            ->orderBy('d.updatedAt', 'DESC')
+            ->getQuery()
+            ->getResult()
+            ;
+    }
 }
